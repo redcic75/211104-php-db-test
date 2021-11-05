10d99498bde57f9d92638e2c4575a6285da609fe
redcic75 <cedric.louyot@gmail.com>
2021-11-05 09:39:02 +0100

    Added possibility to delete a video

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Liste des abonnés</title>
 </head>
 
@@ -30,6 +31,12 @@
     if (isset($_POST['button'])) {
         $req_add = $pdo->prepare('INSERT INTO subscribers VALUES (NULL, ?, ?, ?, ?, NULL)');
         $req_add->execute(array($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['password']));
+    }
+
+    // Delete a purchase
+    if (isset($_POST['sell'])) {
+        $req_sell = $pdo->prepare('DELETE FROM purchases WHERE id_subscriber = ? AND id_video = ? LIMIT 1');
+        $req_sell->execute(array($_POST['id'], $_POST['sell']));
     }
 
     // Add a purchase 
@@ -57,7 +64,7 @@
                 </div>
                 <div class="input col-xs-12 col-sm-6 col-md-3">
                     <label for="lastname" class="form-label">Nom</label>
-                    <input type="text" id="lastname" name="lastname" class="form-control" required>
+                    <input type="text" id="lastname" name="lastname" class="form-control">
                 </div>
                 <div class="input col-xs-12 col-sm-6 col-md-3">
                     <label for="email" class="form-label">E-mail</label>
@@ -94,11 +101,12 @@
             </thead>
             <tbody>
                 <?php
-                // Build $subscribers_array with all data needed to display the html table
+                // Request the database to get all the data needed to display the web page
                 $req_disp = $pdo->prepare('SELECT subscribers.id, subscribers.firstname, subscribers.lastname, subscribers.email, subscribers.password, subscribers.timestamp, GROUP_CONCAT(purchases.id_video ORDER BY purchases.id_video ASC) AS id_video FROM subscribers LEFT JOIN purchases ON subscribers.id = purchases.id_subscriber GROUP BY subscribers.id');
                 $req_disp->execute();
-                $subscribers_array = $req_disp->fetchAll(pdo::FETCH_ASSOC);
+                $subscribers_array = $req_disp->fetchAll(pdo::FETCH_ASSOC); // Store all data to be displayed in an array
                 foreach ($subscribers_array as $row) { ?>
+
                     <tr>
                         <td><?= $row['firstname'] ?></td>
                         <td><?= $row['lastname'] ?></td>
@@ -108,14 +116,26 @@
                         <td><?= $row['id_video'] ?></td>
                         <td>
                             <form action="index.php" method="post">
+
                                 <input type="hidden" name="id" value=<?= $row['id'] ?>>
-                                <button class="btn btn-danger m-1" name="delete" value=''>Supprimer</button>
-                                <!-- TODO: do not display the button to buy a given video when this video has already been bought -->
-                                <button class="btn btn-secondary m-1" name="buy" value=1>Acheter vidéo 1</button>
-                                <button class="btn btn-secondary m-1" name="buy" value=2>Acheter vidéo 2</button>
+                                <button class="btn btn-danger btn-fixed-width m-1" name="delete" value=''>Supprimer</button>
+
+                                <?php
+                                for ($id_video = 1; $id_video <= 2; $id_video++) {
+                                    if (!in_array(strval($id_video), explode(",", $row['id_video']), true)) {
+                                ?>
+                                        <button class="btn btn-secondary btn-fixed-width m-1" name="buy" value=<?= $id_video ?>>Acheter vidéo <?= $id_video ?></button>
+
+                                    <?php } else { ?>
+
+                                        <button class="btn btn-warning btn-fixed-width m-1" name="sell" value=<?= $id_video ?>>Supprimer vidéo <?= $id_video ?></button>
+                                <?php }
+                                } ?>
+
                             </form>
                         </td>
                     </tr>
+
                 <?php } ?>
             </tbody>
         </table>
